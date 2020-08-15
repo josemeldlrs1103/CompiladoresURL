5e773e6a14f6860564eec59908838d4b1afb171d
José Meléndez <josemel1103@gmail.com>
2020-08-15 17:00:59 -0600

    Lectura del archivo .frag

--- Fase 1/fileRW.py
+++ Fase 1/fileRW.py
@@ -1,4 +1,15 @@
 def read(text):
-    error = None
-    tokens = None
-    return tokens, error
+    Extention = list(text)
+    Error = None
+    Tokens = None
+    if(len(Extention)>0):
+        if((Extention[(len(Extention))-4]=='f')and(Extention[(len(Extention))-3]=='r')and(Extention[(len(Extention))-2]=='a')and(Extention[(len(Extention))-1]=='g')):
+            File1= open(text,'r')
+            Tokens= File1.readlines()
+            if((len(Tokens))==0):
+                Error='El archivo está vacío'
+        else:
+            Error = 'La extensión del archivo no es compatible'
+    else:
+        Error = 'Por favor ingrese una dirección'
+    return Tokens, Error
--- Fase 1/main.py
+++ Fase 1/main.py
@@ -2,8 +2,7 @@ import fileRW
 while True:
     text = input('Ingrese la ruta del archivo a analizar\r\n>')
     result, error = fileRW.read(text)
-
     if error: 
-        print(error.as_string())
+        print(error)
     else: 
         print(result)
