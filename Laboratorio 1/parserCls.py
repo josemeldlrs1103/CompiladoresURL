3e5c633ac8b3aed9094cae584a94b7d5b1739afb
unknown <feroliv4z@gmail.com>
2020-09-11 15:05:28 -0600

    añadiendo el resto de metodos faltantes, utlima modificacion a la gramatica

--- Laboratorio 1/parserCls.py
+++ Laboratorio 1/parserCls.py
@@ -105,10 +105,7 @@ class ParserCls:
             self.IfStmtVoid()
         #este bloque define si el stmt es o no epsilon, ya que al no coincidir con ninguno de los simbolos terminales qen los que puede derivar
         #Expr se detecta que se tiene una cadena vacia 
-        elif token in (tokensAndCons.TKN_INTCONST, tokensAndCons.TKN_DOUBCONST, tokensAndCons.TKN_BOOLCONST,tokensAndCons.TKN_STRCONST, tokensAndCons.TKN_NULL,
-                       tokensAndCons.TKN_OR, tokensAndCons.TKN_AND,tokensAndCons.TKN_DBLEQLS, tokensAndCons.TKN_DISTINCT, tokensAndCons.TKN_MINOR, tokensAndCons.TKN_MAJOR,
-                       tokensAndCons.TKN_MAJEQLS, tokensAndCons.TKN_MINEQLS, tokensAndCons.TKN_PLUS, tokensAndCons.TKN_MINUS, tokensAndCons.TKN_MULT, tokensAndCons.TKN_DIV,
-                       tokensAndCons.TKN_PRCTGE, tokensAndCons.TKN_EXCMARK, tokensAndCons.TKN_PAREN_L, tokensAndCons.TKN_IDENTIFIER):
+        elif token in (tokensAndCons.TKN_INTCONST, tokensAndCons.TKN_DOUBCONST, tokensAndCons.TKN_BOOLCONST,tokensAndCons.TKN_STRCONST, tokensAndCons.TKN_NULL, tokensAndCons.TKN_OR, tokensAndCons.TKN_AND,tokensAndCons.TKN_DBLEQLS, tokensAndCons.TKN_DISTINCT, tokensAndCons.TKN_MINOR, tokensAndCons.TKN_MAJOR, tokensAndCons.TKN_MAJEQLS, tokensAndCons.TKN_MINEQLS, tokensAndCons.TKN_PLUS, tokensAndCons.TKN_MINUS, tokensAndCons.TKN_MULT, tokensAndCons.TKN_DIV, tokensAndCons.TKN_PRCTGE, tokensAndCons.TKN_EXCMARK, tokensAndCons.TKN_PAREN_L, tokensAndCons.TKN_IDENTIFIER):
             self.ExprVoid()
 
     def WhileStmtVoid(self):
@@ -138,18 +135,151 @@ class ParserCls:
     def IfStmtVoid_Prime(self):
         token = self.currentToken()
         if token in (tokensAndCons.TKN_ELSE):
+            self.next()
             #si viene un else se vuelve a stmt
             self.StmtVoid ()
 
         
     def ExprVoid(self):
         token = self.currentToken()
-
-    def ConstantVoid(self):
-        token = self.currentToken()
-        if token in (tokensAndCons.TKN_INTCONST, tokensAndCons.TKN_DOUBCONST, 
-                     tokensAndCons.TKN_BOOLCONST,tokensAndCons.TKN_STRCONST, tokensAndCons.TKN_NULL):
+        #evaluar constantes:
+        if token in (tokensAndCons.TKN_INTCONST, tokensAndCons.TKN_DOUBCONST, tokensAndCons.TKN_BOOLCONST,tokensAndCons.TKN_STRCONST, tokensAndCons.TKN_NULL):
             self.next()
+        #evaluar this:
+        elif token in (tokensAndCons.TKN_THIS):
+            self.next()
+        #evaluar new(ident);
+        elif token in (tokensAndCons.TKN_NEW):
+            self.next()
+            token = self.currentToken()
+            if token in (tokensAndCons.TKN_PAREN_L):
+                self.next()
+                token = self.currentToken()
+                if token in (tokensAndCons.TKN_IDENTIFIER):
+                    self.next()
+                    token = self.currentToken()
+                    if token in (tokensAndCons.TKN_PAREN_R):
+                        self.next()
+        #ExprOr
+        elif token in (tokensAndCons.TKN_OR, tokensAndCons.TKN_AND,tokensAndCons.TKN_DBLEQLS, tokensAndCons.TKN_DISTINCT, tokensAndCons.TKN_MINOR, tokensAndCons.TKN_MAJOR, tokensAndCons.TKN_MAJEQLS, tokensAndCons.TKN_MINEQLS, tokensAndCons.TKN_PLUS, tokensAndCons.TKN_MINUS, tokensAndCons.TKN_MULT, tokensAndCons.TKN_DIV, tokensAndCons.TKN_PRCTGE, tokensAndCons.TKN_EXCMARK, tokensAndCons.TKN_PAREN_L, tokensAndCons.TKN_IDENTIFIER):
+            self.ExprOrVoid()
+        #Lvalue
+        else:
+            self.LValueVoid()
+
+    def LValueVoid(self):
+        self.LValueVoid_Prime()
+        self.LValueExprVoid()
+
+    def LValueVoid_Prime(self):
+        token = self.currentToken
+        if token in (tokensAndCons.TKN_IDENTIFIER):
+            self.next()
+        else:
+            self.ExprVoid()
+            if token in (tokensAndCons.TKN_DOT):
+                self.next()
+                token = self.currentToken
+                if token in (tokensAndCons.TKN_IDENTIFIER):
+                    self.next()
+            elif token in (tokensAndCons.TKN_SQRBRKT_L):
+                self.next()
+                self.ExprVoid()
+                token = self.currentToken
+                if token in (tokensAndCons.TKN_SQRBRKT_R):
+                    self.next()
+    
+    def LValueExprVoid(self):
+        self.LValueVoid()
+        token = self.currentToken
+        if token in (tokensAndCons.TKN_EQUALS):
+            self.next()
+            self.ExprVoid()
+
+    def ExprOrVoid(self):
+        self.ExprAndVoid()
+        self.ExprOrVoid_Prime()
+
+    def ExprOrVoid_Prime(self):
+        token = self.currentToken
+        if token in (tokensAndCons.TKN_OR):
+            self.next()
+            self.ExprAndVoid()
+            self.ExprOrVoid_Prime()
+        
+    def ExprAndVoid(self):
+        self.ExprEqualsVoid()
+        self.ExprAndVoid_Prime()
+
+    def ExprAndVoid_Prime(self):
+        token = self.currentToken
+        if token in (tokensAndCons.TKN_AND):
+            self.next()
+            self.ExprEqualsVoid()
+            self.ExprAndVoid_Prime()
+
+    def ExprEqualsVoid(self):   
+        self.ExprCompVoid()
+        self.ExprEqualsVoid_Prime()
+
+    def ExprEqualsVoid_Prime(self):
+        token = self.currentToken
+        if token in (tokensAndCons.TKN_DBLEQLS, tokensAndCons.TKN_DISTINCT):
+            self.next()
+            self.ExprCompVoid()
+            self.ExprEqualsVoid_Prime()
+
+    def ExprCompVoid(self):
+        self.ExprAddVoid()
+        self.ExprCompVoid_Prime()
+
+    def ExprCompVoid_Prime(self):
+        token = self.currentToken
+        if token in (tokensAndCons.TKN_MAJEQLS, tokensAndCons.TKN_MINEQLS, tokensAndCons.TKN_MINOR, tokensAndCons.TKN_MAJOR):
+            self.next()
+            self.ExprAddVoid()
+            self.ExprCompVoid_Prime()
+
+    def ExprAddVoid(self):
+        self.ExprMulVoid()
+        self.ExprAddVoid_Prime()
+
+    def ExprAddVoid_Prime(self):
+        token = self.currentToken
+        if token in (tokensAndCons.TKN_PLUS, tokensAndCons.TKN_MINUS):
+            self.next()
+            self.ExprMulVoid()
+            self.ExprAddVoid_Prime()
+
+    def ExprMulVoid(self):
+        self.ExprPreVoid()
+        self.ExprMulVoid_Prime()
+
+    def ExprMulVoid_Prime(self):
+        token = self.currentToken
+        if token in (tokensAndCons.TKN_MULT, tokensAndCons.TKN_DIV, tokensAndCons.TKN_PRCTGE):
+            self.next()
+            self.ExprPreVoid()
+            self.ExprMulVoid_Prime()
+
+    def ExprPreVoid(self):
+        self.ExprParenVoid()
+        self.ExprPreVoid_Prime()
+
+    def ExprPreVoid_Prime(self):
+        token = self.currentToken
+        if token in (tokensAndCons.TKN_MINUS, tokensAndCons.TKN_EXCMARK):
+            self.next()
+            self.ExprParenVoid()
+            self.ExprPreVoid_Prime()
+
+    def ExprParenVoid(self):
+        token = self.currentToken
+        if token in (tokensAndCons.TKN_PAREN_L):
+            self.next()
+            self.ExprVoid()
+            if token in (tokensAndCons.TKN_PAREN_R):
+                self.next()
 
     def FormalsVoid(self):
         self.VariableVoid()
@@ -172,7 +302,7 @@ class ParserCls:
 
     def TypeArrayVoid(self):
         self.TypeVoid()
-        #se evalua que despues de el tipo venga un identificador
+        # se evalua que despues de el tipo venga un identificador
         token = self.currentToken
         if token in (tokensAndCons.TKN_IDENTIFIER):
             self.next()
@@ -191,4 +321,3 @@ class ParserCls:
         if token in (tokensAndCons.TKN_SQRBRKT):
             self.next()
         ## si no se detecta como epsilon y no se avanza 
-  
